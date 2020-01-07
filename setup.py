ba9ef87efd31011bf5a0a913c89b5c841072f4a3
Peter Geer <pgeer@datto.com>
2020-01-06 21:49:21 -0500

    Update info in setup.py and add version restriction

--- setup.py
+++ setup.py
@@ -14,6 +14,9 @@ import os
 import sys
 from distutils.core import setup
 
+if sys.version_info < (3, 0):
+    sys.exit("Sorry, Python 3.0 or greater is required")
+
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "lib"))
 try:
     import go
@@ -26,26 +29,18 @@ Environment :: Console
 Intended Audience :: Developers
 License :: OSI Approved :: MIT License
 Operating System :: OS Independent
-Programming Language :: Python :: 2
+Programming Language :: Python :: 3
 Topic :: Software Development :: Libraries :: Python Modules
 """
-
-if sys.version_info < (2, 3):
-    # Distutils before Python 2.3 doesn't accept classifiers.
-    _setup = setup
-    def setup(**kwargs):
-        if kwargs.has_key("classifiers"):
-            del kwargs["classifiers"]
-        _setup(**kwargs)
 
 doclines = __doc__.split("\n")
 
 setup(
     name="go",
     version=go.__version__,
-    maintainer="Trent Mick",
-    maintainer_email="trentm@gmail.com",
-    url="http://code.google.com/p/go-tool/",
+    maintainer="Peter Geer",
+    maintainer_email="pageer@skepticats.com",
+    url="http://github.com/pageer/go-/posh",
     license="http://www.opensource.org/licenses/mit-license.php",
     platforms=["any"],
     py_modules=["go"],
